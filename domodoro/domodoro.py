05882224127a668240a539abed6e47b43e472d9a
liz-lovelace <ivanov0604041@gmail.com>
2023-06-13 15:35:31 +0600

    edit domodoro time

--- domodoro/domodoro.py
+++ domodoro/domodoro.py
@@ -4,9 +4,9 @@ import sys
 from time import time, sleep
 import os
 
-work_minutes = 25
+work_minutes = 45
 break_minutes = 15
-long_break_minutes = 20
+long_break_minutes = 30
 
 status_path = '/tmp/domodoro.json'
 
